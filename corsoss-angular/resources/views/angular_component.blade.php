97e3acbf87d462a546bfdf8fd878d983faed0211
soluzione-software <info@soluzionesoftware.com>
2024-04-05 09:01:36 +0200

    modifica @vite non funziona bene

--- corsoss-angular/resources/views/angular_component.blade.php
+++ corsoss-angular/resources/views/angular_component.blade.php
@@ -11,7 +11,7 @@
         <app-root></app-root> <!-- Selettore del componente Angular -->
     </div>
     <!-- Includi i bundle di Vite o Angular -->
-    @vite(['corsoss-angular/src/main.ts'])
+    {{-- @vite(['corsoss-angular/src/main.ts']) --}}
     <script type="module" src="{{asset('corsoss-angular/browser/polyfills-RT5I6R6G.js')}}"></script>
     <script type="module" src="{{asset('corsoss-angular/browser/main-C3CCRMEV.js')}}"></script>
     {{-- <script type="module" src="./corsoss-angular/src/main.ts"></script> --}}
