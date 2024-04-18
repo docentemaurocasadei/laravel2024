4204ea00ef6d63e3f69d2d5cfdbe0034a0b365a1
soluzione-software <info@soluzionesoftware.com>
2024-04-18 11:41:34 +0200

    readme

---
+++ lezioni/lezione5/app/Http/Controllers/CarController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    //per ritornare 1 modello con il suo brand
+    //Car::with('brand')->where('id',1)->get()
+    //get serve per trasformare la query in una collection (dati)
+    //all(), find(), first(), findOrFail(), firstOrFail() -> non richiedono la get()
+    //where(), with() .. richiedono la get()
+    //per ritornare tutti i modelli
+    //Car::all();
+    //per ritornare tutti i modelli con il brand
+    //Car::with('brand')->get()
+    //per recuperare tutti i modelli con hp > 20 e brand = 1
+    //Car::query()->where('brand_id', 1)->where('hp', '>', '20')->get()
+    //per creare un modello
+    //Car::create(['name' => 'Classe A', 'hp' => '115', 'brand_id' => 1]);
+/*
+ $classeA = App\Models\Car::find(5)                                                            
+= App\Models\Car {#6006
+    id: 5,
+    name: "Classe A",
+    hp: "115",
+    brand_id: 1,
+    created_at: "2024-04-18 09:05:20",
+    updated_at: "2024-04-18 09:05:20",
+  }
+
+> $classeA->hp = '125'
+= "125"
+
+> $classeA->save()
+= true
+*/
+/*
+App\Models\Car::where('id',5)->update(['hp' => '125']);
+*/
+/*
+$classeA = App\Models\Car::find(5)                                                            
+= App\Models\Car {#6006
+    id: 5,
+    name: "Classe A",
+    hp: "115",
+    brand_id: 1,
+    created_at: "2024-04-18 09:05:20",
+    updated_at: "2024-04-18 09:05:20",
+  }
+
+> $classeA->delete()
+= true
+*/
+/*
+App\Models\Car::where('id',5)->delete();
+*/
+/**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'cars' =>
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- lezioni/lezione5/app/Models/Car.php
+++ lezioni/lezione5/app/Models/Car.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Car extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'hp', 'brand_id'];
+    //relazioni
+    //one to one HasOne
+    //one to many HasMany | BelongsTo
+    //many to many BelongsToMany
+    public function brand():BelongsTo {
+        return $this->belongsTo(Brand::class);
+    }
 }
